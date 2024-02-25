d8b99516cf1da4784146fd06629f2b32f7468124
mariocalipo <mariohtavaresc@live.com>
2024-02-24 23:02:14 -0300

    adapt the code

--- provider.tf
+++ provider.tf
@@ -1,4 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
 provider "google" {
+
   project = "challenge-devsecops-sre"
   region  = "us-central1"
 }
