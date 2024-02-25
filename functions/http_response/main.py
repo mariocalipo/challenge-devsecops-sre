d69815d68c36caa53d0b4e336771ea71ae76744e
mariocalipo <mariohtavaresc@live.com>
2024-02-25 20:28:36 -0300

    adapt the code

--- functions/http_response/main.py
+++ functions/http_response/main.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add the parent directory to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from bigquery_query.main import bq_query
 from flask import jsonify
 
