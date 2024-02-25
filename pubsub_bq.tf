d31aeb183b784cde79bbf06669748a01115890ad
mariocalipo <mariohtavaresc@live.com>
2024-02-25 18:23:07 -0300

    adapt the code

--- pubsub_bq.tf
+++ pubsub_bq.tf
@@ -1,5 +1,5 @@
-module "pubsub-01" {
-  source           = "./modules/pubsub"
+module "pubsub-bq-01" {
+  source           = "./modules/pubsub_bq"
   bigquery_dataset     = var.config.bigquery-01.bigquery_dataset
   bigquery_table = var.config.bigquery-01.bigquery_table
   pubsub_topic     = var.config.pubsub-01.topic
