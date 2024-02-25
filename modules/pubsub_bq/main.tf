c106ce06a6723e8961f9d2ae1ba5a7ff3c9935c3
mariocalipo <mariohtavaresc@live.com>
2024-02-25 18:44:50 -0300

    adapt the code

--- modules/pubsub_bq/main.tf
+++ modules/pubsub_bq/main.tf
@@ -42,17 +42,16 @@ resource "google_pubsub_subscription" "main" {
   depends_on = [google_project_iam_member.viewer, google_project_iam_member.editor]
 }
 
-data "google_project" "project" {
-}
+// IAM entries
 
 resource "google_project_iam_member" "viewer" {
-  project = data.google_project.project.project_id
+  project = data.google_project.current.project_id
   role   = "roles/bigquery.metadataViewer"
-  member = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
+  member = "serviceAccount:service-${data.google_project.current.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "editor" {
-  project = data.google_project.project.project_id
+  project = data.google_project.current.project_id
   role   = "roles/bigquery.dataEditor"
-  member = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
+  member = "serviceAccount:service-${data.google_project.current.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
 }
